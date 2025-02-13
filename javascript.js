5985010cdce3922fed0c2b1a2b8d0cf03e03c577
clut19 <vgonzalezseb@gmail.com>
2025-02-13 22:20:44 +0100

    Create initial files and basic functions
    Create initial html, javascript and css files and also make
    the basic mathematic functions.

---
+++ javascript.js
@@ -0,0 +1,21 @@
+function sum (a,b){
+    return a+b;
+}
+
+function substract (a,b){
+    return a-b;
+}
+
+function multiply (a,b){
+    return a*b;
+}
+
+function divide (a,b){
+    return a/b;
+}
+
+
+console.log(sum(1,2));
+console.log(substract(1,2));
+console.log(multiply(2,3));
+console.log(divide(9,3));
