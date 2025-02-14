de8b9cfd0183dea366e43103fd402a5e4c766061
clut19 <vgonzalezseb@gmail.com>
2025-02-14 20:33:45 +0100

    Make display functional with digits

--- javascript.js
+++ javascript.js
@@ -14,8 +14,22 @@ function divide (a,b){
     return a/b;
 }
 
+function operate (a,b, func){
+    return func(a,b);
+}
 
-console.log(sum(1,2));
-console.log(substract(1,2));
-console.log(multiply(2,3));
-console.log(divide(9,3));
+let firstNumber;
+let secondNumber;
+let operator;
+let currentValueDisplay;
+
+
+const digits = document.querySelectorAll(".digit");
+const display = document.querySelector("#display");
+
+digits.forEach ((x) => {
+    x.addEventListener ("click", () => {
+        display.textContent += x.getAttribute("id");
+        currentValueDisplay = parseFloat(display.textContent);
+    });
+});
